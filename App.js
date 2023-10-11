7a888f6f22ee86f0922e63cbd75c4227a1aa021f
김수린 <uhyeon1009@gmail.com>
2023-10-12 01:25:28 +0900

    fix: stylesheet 위치 변경

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { RootApp } from './src/RootApp';
 import { NavigationContainer } from '@react-navigation/native';
 import { RecoilRoot } from 'recoil';
-import { StyleSheet } from 'react-native';
 
 export default function App() {
   return (
@@ -15,12 +14,3 @@ export default function App() {
     </RecoilRoot>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/screens/MainBottomTabs/MainScreen.js
+++ src/screens/MainBottomTabs/MainScreen.js
@@ -1,13 +1,22 @@
 import { View, Text, SafeAreaView } from 'react-native';
 import { MainHeader } from '../../components/Headers/Headers';
+import { StyleSheet } from 'react-native';
+import * as Color from '../../components/Colors/colors';
 
 const MainScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <MainHeader />
       <Text>MainScreen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Color.White_100,
+  },
+});
 
 export default MainScreen;
