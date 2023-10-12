fac8544d776ef676e69c226990e7bfcb2ceadcce
김수린 <uhyeon1009@gmail.com>
2023-10-12 22:49:39 +0900

    fix: textinput 시 키보드 문제 확인

--- src/screens/MainBottomTabs/StarScreen.js
+++ src/screens/MainBottomTabs/StarScreen.js
@@ -2,12 +2,15 @@ import { View, Text, StyleSheet } from 'react-native';
 import { MainHeader } from '../../components/Headers/Headers';
 import * as Color from '../../components/Colors/colors';
 import { Body } from '../../components/Typography/Typography';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 const StarScreen = () => {
   return (
     <View style={styles.container}>
       <MainHeader />
-      <Body>StarScreen</Body>
+      <KeyboardAwareScrollView>
+        <Body>StarScreen</Body>
+      </KeyboardAwareScrollView>
     </View>
   );
 };
