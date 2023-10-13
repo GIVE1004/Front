a85604c8bd372cf4e9cd080e5c626c429cf98c10
김수린 <uhyeon1009@gmail.com>
2023-10-14 01:59:14 +0900

    fix: footer수정, gradlew접근 허용

--- src/components/footers/footers.js
+++ src/components/footers/footers.js
@@ -1,23 +1,21 @@
 import { View, useWindowDimensions } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import * as Color from '../Colors/colors';
-import { BasicButton } from '../Buttons/Buttons';
 
 export const Footer = (props) => {
   const insets = useSafeAreaInsets();
   const { width } = useWindowDimensions();
   return (
-    <View style={{ backgroundColor: Color.Black_20, paddingTop: insets.top }}>
+    <View style={{ backgroundColor: Color.Black_20, paddingVertical: 10 }}>
       <View
         style={{
           width: width,
           flexDirection: 'row',
-          height: Platform.OS === 'android' ? 68 : 60,
           borderBottomColor: Color.Black_20,
           borderBottomWidth: 1,
           alignItems: 'center',
-          justifyContent: 'space-between',
-          paddingHorizontal: 20,
+          justifyContent: 'space-evenly',
+          paddingHorizontal: 10,
         }}
       >
         {props.children}
