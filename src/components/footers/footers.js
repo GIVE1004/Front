9efe466419a211fc33288371a054b50535d656cb
김수린 <uhyeon1009@gmail.com>
2023-10-14 13:54:39 +0900

    feat: modal 및 footer Component 완료

--- src/components/footers/footers.js
+++ src/components/footers/footers.js
@@ -3,16 +3,15 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import * as Color from '../Colors/colors';
 
 export const Footer = (props) => {
-  const insets = useSafeAreaInsets();
   const { width } = useWindowDimensions();
+  const insets = useSafeAreaInsets();
   return (
-    <View style={{ backgroundColor: Color.Black_20, paddingVertical: 10 }}>
+    <View style={{ backgroundColor: props.backgroundColor || Color.Black_20, paddingBottom: insets.bottom }}>
       <View
         style={{
           width: width,
+          margin: 5,
           flexDirection: 'row',
-          borderBottomColor: Color.Black_20,
-          borderBottomWidth: 1,
           alignItems: 'center',
           justifyContent: 'space-evenly',
           paddingHorizontal: 10,
--- src/screens/MainBottomTabs/MainScreen.js
+++ src/screens/MainBottomTabs/MainScreen.js
@@ -5,25 +5,24 @@ import { Body, Heading } from '../../components/Typography/Typography';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { BasicButton } from '../../components/Buttons/Buttons';
 import { Footer } from '../../components/Footers/Footers';
+import { useNavigation } from '@react-navigation/native';
 
 const MainScreen = () => {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <MainHeader />
       <KeyboardAwareScrollView>
         <Heading>MainScreen</Heading>
-        <BasicButton>
+        <BasicButton
+          borderColor={Color.Secondary_50}
+          onPress={() => {
+            navigation.navigate('GroupDetailScreen');
+          }}
+        >
           <Body>route test</Body>
         </BasicButton>
       </KeyboardAwareScrollView>
-      <Footer>
-        <BasicButton borderRadius={14} width={140} borderColor={Color.Primary_50} backgroundColor={Color.White_100}>
-          <Body>취소</Body>
-        </BasicButton>
-        <BasicButton borderRadius={14} width={140} borderColor={Color.Primary_50} backgroundColor={Color.Primary_50}>
-          <Body>확인</Body>
-        </BasicButton>
-      </Footer>
     </View>
   );
 };
