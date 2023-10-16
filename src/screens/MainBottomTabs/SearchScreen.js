7b1a49f978b3447dd517bc54dd028750408b2b42
jeonwonbhin <wjssoqls@gachon.ac.kr>
2023-10-16 23:36:39 +0900

    fix: searchScreen

--- src/screens/MainBottomTabs/SearchScreen.js
+++ src/screens/MainBottomTabs/SearchScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, KeyboardAvoidingView} from 'react-native';
-import {Spacer} from '../../components/Basic/Spacer';
+import { View, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import { Spacer } from '../../components/Basic/Spacer';
 import { SearchHeader } from '../../components/Headers/Headers';
 import * as Color from '../../components/Colors/colors';
 import { Body } from '../../components/Typography/Typography';
@@ -20,15 +20,50 @@ const SearchScreen = () => {
           console.log('press Search');
         }}
       />
-        <KeyboardAwareScrollView>
-          <View style={{width:'100%', paddingHorizontal:15}}>
-          <Spacer space={10}/>
-          <Body fontWeight={'bold'}>기부단체 검색</Body>  
-          <Spacer space={10}/>
+      <KeyboardAwareScrollView>
+        <View style={{ width: '100%', paddingHorizontal: 15 }}>
+          <Spacer space={10} />
+          <Body fontWeight={'bold'}>기부단체 검색</Body>
+          <Spacer space={10} />
           <SwiftLabel isFocus={isFocus} setIsFocus={setIsFocus} labels={labels} />
-          <SearchGroupCard/>
-          </View>
-        </KeyboardAwareScrollView>
+          {selectedLabel === '지금 뜨는' && <HotNow />}
+          {selectedLabel === '높은 신뢰도' && <HighValidity />}
+          {selectedLabel === '작은 기부단위' && <SmallBudget />}
+          {selectedLabel === '활발한 활동' && <Activity />}
+        </View>
+      </KeyboardAwareScrollView>
+    </View>
+  );
+};
+
+export const HotNow= () => {
+  return (
+    <View>
+      <SearchGroupCard/>
+    </View>
+  );
+};
+
+export const HighValidity =() =>{
+  return(
+    <View>
+      <SearchGroupCard/>
+    </View>
+  );
+};
+
+export const SmallBudget=() =>{
+  return(
+    <View>
+      <SearchGroupCard/>
+    </View>
+  );
+};
+
+export const Activity =() =>{
+  return(
+    <View>
+      <SearchGroupCard/>
     </View>
   );
 };
@@ -37,7 +72,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Color.White_100,
-    alignItems:'center',
+    alignItems: 'center',
   },
 });
 
