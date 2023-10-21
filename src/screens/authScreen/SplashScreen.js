a3ac3ddf53242931c0d089c279cbb2f2c5a658b7
uhyeon19 <51315222+uhyeon19@users.noreply.github.com>
2023-10-21 18:28:21 +0900

    feat: 자동로그인 처리 중 (#52)
    * feat: login로직 작업 중
    
    * feat: 자동 로그인 처리 중. API 바뀌면 TODO 처리하기

--- src/screens/authScreen/SplashScreen.js
+++ src/screens/authScreen/SplashScreen.js
@@ -1,23 +1,54 @@
+import { useEffect, useState } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { useWindowDimensions, View } from 'react-native';
 import { LocalImageLoader } from '../../components/Images/ImageLoader';
 import { Logo } from '../../components/Images/Logo';
 import { Header } from '../../components/Headers/Headers';
 import { Spacer } from '../../components/Basic/Spacer';
-import { Body, Heading } from '../../components/Typography/Typography';
+import { Heading } from '../../components/Typography/Typography';
 import { BasicButton } from '../../components/Buttons/Buttons';
 import * as Color from '../../components/Colors/colors';
 import { useRecoilState } from 'recoil';
 import { goMainPageState } from '../../util/recoil/Atoms';
 import { useNavigation } from '@react-navigation/native';
-import { getAuthRedirectFetch } from '../../util/fetch/fetchUtil';
+import { getTokens, setTokens } from '../../util/token/tokenUtil';
+import { getRefreshFetch } from '../../util/fetch/fetchUtil';
 
 const SplashScreen = () => {
   const navigation = useNavigation();
   const { width } = useWindowDimensions();
   const [goMainPage, setGoMainPage] = useRecoilState(goMainPageState);
+  const [accessToken, setAccessToken] = useState('');
+  const [refreshToken, setRefreshToken] = useState('');
 
-  const onPressStart = () => {};
+  const getRefreshData = async () => {
+    try {
+      const response = await getRefreshFetch(accessToken, refreshToken);
+      const data = await response.json();
+      setAccessToken('');
+      setRefreshToken('');
+      if (data.dataHeader.successCode == 0) {
+        setTokens(data.dataBody.accessToken, data.dataBody.refreshToken);
+        // TO-DO
+        // API달라졌을 때, memberInfo 오게되는 경우 처리하기
+        setGoMainPage(true);
+      } else {
+        navigation.navigate('OauthScreen');
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    if (accessToken !== null && refreshToken !== null && accessToken !== '' && refreshToken !== '') {
+      getRefreshData();
+    } else if (accessToken == null && refreshToken == null) {
+      navigation.navigate('OauthScreen');
+      setAccessToken('');
+      setRefreshToken('');
+    }
+  }, [accessToken, refreshToken]);
 
   return (
     <SafeAreaProvider style={{ flex: 1, padding: 14, backgroundColor: Color.White_100 }}>
@@ -34,7 +65,7 @@ const SplashScreen = () => {
         backgroundColor={Color.Primary_50}
         borderColor={Color.Primary_50}
         onPress={() => {
-          navigation.navigate('OauthScreen');
+          getTokens(setAccessToken, setRefreshToken);
         }}
       >
         <Heading fontSize={16}>시작하기</Heading>
