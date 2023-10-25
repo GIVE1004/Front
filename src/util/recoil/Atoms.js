8aa4c9543bbe035e636c140fdc1b419e079111ad
김수린 <uhyeon1009@gmail.com>
2023-10-25 23:22:45 +0900

    chore: chatbot 구상에 대한 논의 필요

--- src/util/recoil/Atoms.js
+++ src/util/recoil/Atoms.js
@@ -9,3 +9,8 @@ export const memberInfoState = atom({
   key: 'memberInfoState',
   default: null,
 });
+
+export const chatbotState = atom({
+  key: 'chatbotState',
+  default: [{ bot: '안녕하세요. GIVE천사 봇입니다. 궁금한 점을 도와드리겠습니다.' }, { user: '저는 기부를 처음해봐요. 어떤 업체가 좋을까요?' }],
+});
